a88adc59accee46a517cde5239bc766a8dc50360
Eric John <38982708+ejllovido@users.noreply.github.com>
2019-12-19 21:20:44 +0800

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,13 @@ import SocialMediaAlternate from "./common/SocialMedialAlternate";
 const Footer = () => {
   return (
     <footer className="main-footer">
-      <p className="text">Designed and developed in Manila, Philippines</p>
+      <p className="text">
+        Designed and developed in Manila, Philippines
+        <br />
+        <span className="attribution">
+          Huge thanks to Inter UI for the font
+        </span>
+      </p>
       <SocialMediaAlternate />
     </footer>
   );

29a6180b6dce039d81c3ef7279e4372d60abc082
Eric John <38982708+ejllovido@users.noreply.github.com>
2019-12-19 08:42:33 +0800

    attempt 1 for Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+
+  const handleClick = () => {
+    setActive(active === "" ? "link-active" : "");
+  };
+
   return (
     <nav className="main-nav">
       <Link to="/" className="title">
         EJ
       </Link>
       <div className="linkset">
-        <Link to="/" className="link">
+        <Link to="/" className={`link ${setActive}`} onClick={handleClick}>
           Home
         </Link>
-        <Link to="/showcase" className="link">
+        <Link to="/showcase" className="link" onClick={handleClick}>
           Showcase
         </Link>
-        <Link to="/exercise" className="link">
+        <Link to="/exercise" className="link" onClick={handleClick}>
           Exercise
         </Link>
       </div>
